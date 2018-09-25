5456f7fc85d20c6e26adf52f14279d85b69770a5
lmml-l <43039968+lmml-l@users.noreply.github.com>
2018-09-25 18:04:20 +0200

    Menu js
    Se ha creado el archivo js encargado del menu

---
+++ Fight for the hood/menu.js
@@ -0,0 +1,12 @@
+class MainMenu extends Phaser.Scene {
+	constructor(){
+		super({key:"MainMenu"});
+	}
+	preload(){
+		this.load.image('title','');
+		this.load.image('buttonlocal',);
+		this.load.image('buttononline',)
+	}
+
+
+}
