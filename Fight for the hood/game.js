93d6671e051e469b9e91483ce86f6dfe011f762a
lmml-l <43039968+lmml-l@users.noreply.github.com>
2018-09-26 19:04:55 +0200

    26/09/2018
    Se ha avanzado en el menu principal

--- Fight for the hood/game.js
+++ Fight for the hood/game.js
@@ -2,11 +2,8 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [MainMenu]
+         
 };
 
 var game = new Phaser.Game(config);
@@ -17,6 +14,7 @@ function preload ()
 
 function create ()
 {
+
 }
 
 function update ()
--- Fight for the hood/menu.js
+++ Fight for the hood/menu.js
@@ -3,11 +3,17 @@ class MainMenu extends Phaser.Scene {
 		super({key:"MainMenu"});
 	}
 	preload(){
-		this.load.image('title','Recursos/Imagenes/Logo.png');
-		this.load.image('buttonlocal','');
-		this.load.image('buttononline','')
-		this.load.image('controles','')
+		var title = this.load.image('title','Logo.png');
+		var buttonlocal = this.load.image('buttonlocal','Recursos/Imagenes/plain-button-500x500.png');
+		var buttononline = this.load.image('buttononline','Recursos/Imagenes/plain-button-500x500.jpg')
+		var controles = this.load.image('controles','Recursos/Imagenes/plain-button-500x500.jpg')
 	}
-
-
+	//this.add.image(400,300,)
+	create(){
+		var title = this.add.image(300,400,'title');
+		var buttonlocal = this.add.image(600,300,'buttonlocal');
+		var buttononline = this.add.image(700,200,'buttononline');
+		var controles = this.add.image(800,200,'controles');
+		console.log("reading this");
+	}
 }
