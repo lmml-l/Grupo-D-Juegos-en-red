5fbb7fce5d1c7303fc4a519f5fe741dfeb9f1dd6
lmml- <43039968+lmml-l@users.noreply.github.com>
2018-09-28 22:14:58 +0200

    28/09/2018 menu
    Se sigue progresando con la clase menu que hereda de la clase escena definida por phaser

--- Fight for the hood/menu.js
+++ Fight for the hood/menu.js
@@ -2,18 +2,25 @@ class MainMenu extends Phaser.Scene {
 	constructor(){
 		super({key:"MainMenu"});
 	}
-	preload(){
-		var title = this.load.image('title','Recursos/Imagenes/Logo.png');
-		var buttonlocal = this.load.image('buttonlocal','Recursos/Imagenes/plain-button-500x500.png');
-		var buttononline = this.load.image('buttononline','Recursos/Imagenes/plain-button-500x500.jpg')
-		var controles = this.load.image('controles','Recursos/Imagenes/plain-button-500x500.jpg')
-	}
-	//this.add.image(400,300,)
-	create(){
-		var title = this.add.image(300,400,'title');
-		var buttonlocal = this.add.image(600,300,'buttonlocal');
-		var buttononline = this.add.image(700,200,'buttononline');
-		var controles = this.add.image(800,200,'controles');
+	
+	menu(){
+		var title = this.add.image((this.game.canvas.width/2),(this.game.canvas.height/2),'title').setScale(1);
+		//var buttonlocal = this.add.image(600,300,'buttonlocal');
+		//var buttononline = this.add.image(700,200,'buttononline');
+		//var controles = this.add.image(800,200,'controles');
 		console.log("reading this");
 	}
+	preload(){
+		var title = this.load.image('title','Recursos/Imagenes/Logo.png')
+		var buttonlocal = this.load.image('buttonlocal','Recursos/Imagenes/plain-button-500x500.jpg');
+		var buttononline = this.load.image('buttononline','Recursos/Imagenes/plain-button-500x500.jpg');
+		var controles = this.load.image('controles','Recursos/Imagenes/plain-button-500x500.jpg');
+	}
+	//this.add.image(400,300,)
+
+	create(){
+		this.menu();
+	}
+
+	
 }
