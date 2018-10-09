b73c2e73d37e0772b65a77587a9c3cdca35c7146
FernandoMoreno98 <43215893+FernandoMoreno98@users.noreply.github.com>
2018-10-09 21:24:46 +0200

    Update Escenario.js prueba de plataformas
    He añadido plataformas provisionales de prueba

--- Fight for the hood/Escenario.js
+++ Fight for the hood/Escenario.js
@@ -8,26 +8,57 @@ class MainEscenario extends Phaser.Scene {
 
 preload(){
 
-	this.load.image('Fondo', 'assets/background.png'); 
-	this.load.image('Suelo', '');
+	this.load.image('Fondo', 'imagenes/stage.png');
+	this.load.image('Plat2', 'assets/plat2.png'); //atravesables
 
 
 }
 
 create(){
-	var platformas;
-	var Suelo;
+	var plataformas;
+	var plataformasMetal;
+	var suelo;
 
 
 		//this.Escenario(); (No se sabe si funciona así)
 		this.add.sprite(0, 0, 'Fondo');
-		this.add.sprite(0, 0, 'Suelo');
+		
+		//this.add.sprite(0, 0, 'Plat2')
 
-	platformas = this.physics.add.staticGroup();  //Hace solidas las plataformas
+	plataformas = this.physics.add.staticGroup();  //Hace solidas las plataformas
+	plataformasMetal = this.physics.add.staticGroup();
 
-    platformas.create(x, y, 'Suelo').setScale(2).refreshBody();
+	//las plataformas de metal no son colisionables desde abajo
+	this.plataformasMetal.setAll('body.collideDown', false);
 
-    platformas.create(x, y, 'Suelo');
+
+	//Suelo
+    //platformas.create(x, y, 'suelo').setScale(2).refreshBody();
+    plataformas.create(0, 696, 'suelo');
 
 
+    //LAS COORDENADAS SON RESPECTO A LA VENTANA DEL TAMAÑO DEL ESCENARIO
+    //coloco plataformas fijas no atravesables (techos)
+    //aquí
+
+    //coloco plataformas fijas atravesables
+    //escaleras de emergencia
+    //izquierda
+    plataformasMetal.create(263, 224, 'plat2');
+    plataformasMetal.create(263, 314, 'plat2');
+    plataformasMetal.create(263, 402, 'plat2');
+    plataformasMetal.create(263, 489, 'plat2');
+    plataformasMetal.create(263, 577, 'plat2');
+    //derecha
+    plataformasMetal.create(803, 301, 'plat2');
+    plataformasMetal.create(803, 389, 'plat2');
+    plataformasMetal.create(803, 564, 'plat2');
+    //sueltas
+    plataformasMetal.create(415, 436, 'plat2');
+    plataformasMetal.create(480, 522, 'plat2');
+    plataformasMetal.create(480, 628, 'plat2');
+    plataformasMetal.create(646, 436, 'plat2');
+    plataformasMetal.create(647, 524, 'plat2');
 	}
+
+	//QUEDAN MÁS PLATAFORMAS POR PONER, DE DISTINTOS TAMAÑOS
