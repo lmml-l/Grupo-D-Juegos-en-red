763d7c2d5bb19283008e7ff8a0f8759abcda3620
FernandoMoreno98 <fernando.morenodiaz@hotmail.com>
2018-10-10 22:00:13 +0200

    Plataformas y colisiones
    Adición de plataformas con colisión.
    
    Sprites visibles de las plataformas, colisión con ellas y con el suelo.

--- Fight for the hood/Escenario.js
+++ Fight for the hood/Escenario.js
@@ -4,7 +4,7 @@ class MainEscenario extends Phaser.Scene {
 		super({key:"MainEscenario"});
         var sprite = ["Recursos/Imagenes/Sprite2.png","Recursos/Imagenes/Sprite2.png"]
         this.avatar = new Avatar("a",this,400,400,sprite);
-        this.jugador = new Jugador(this.avatar,controles1);
+        this.jugador = new Jugador(this.avatar,controles2);
 	}
 
 
@@ -23,7 +23,9 @@ preload(){
     this.load.image('cartelPixel', 'Recursos/Imagenes/cartelPixel.png');
     this.load.image('toldoLPixel', 'Recursos/Imagenes/toldoLPixel.png');
     this.load.image('toldoRPixel', 'Recursos/Imagenes/toldoRPixel.png');
-    this.load.image('tuboPixel', 'Recursos/Imagenes/stage.png');
+    this.load.image('tuboPixel', 'Recursos/Imagenes/tuboPixel.png');
+    this.load.image('tanquePixel', 'Recursos/Imagenes/tanquePixel.png');
+    this.load.image('paredPixel', 'Recursos/Imagenes/paredPixel.png');
 
 }
 
@@ -32,6 +34,8 @@ create(){
 
 	var plataformas;
 	var suelo;
+    var tanque;
+    var pared;
 
     //IMPORTANTE: LA POSICIÓN (0,0) ES EL CENTRO DEL ESCENARIO.
     //            EN PHOTOSHOP ES LA ESQUINA SUPERIOR IZQUIERDA
@@ -45,11 +49,9 @@ create(){
 	plataformas = this.physics.add.staticGroup();  //Hace solidas las plataformas
     //plataformasCortas = this.physics.add.staticGroup();
     suelo = this.physics.add.staticGroup();
+    tanque = this.physics.add.staticGroup();
+    pared = this.physics.add.staticGroup();
     
- 
-	//las plataformas de metal no son colisionables desde abajo
-	//this.plataformasMetal.setAll('body.collideDown', false);
-
 
 	//Suelo
     //platformas.create(x, y, 'suelo').setScale(2).refreshBody();
@@ -57,10 +59,7 @@ create(){
 
 
     //LAS COORDENADAS SON RESPECTO A LA VENTANA DEL TAMAÑO DEL ESCENARIO
-    //coloco plataformas fijas no atravesables (techos)
-    //aquí
 
-    //coloco plataformas fijas atravesables
     //escaleras de emergencia
     //izquierda 
     plataformas.create(319, 63,  'plat2');
@@ -74,23 +73,46 @@ create(){
     plataformas.create(866, 317, 'plat2');
     plataformas.create(866, 404, 'plat2');
     //sueltas
-    plataformas.create(455, 276, 'platShort'); //-16
+    plataformas.create(455, 276, 'platShort');
     plataformas.create(520, 362, 'platShort');
     plataformas.create(520, 468, 'platShort');
     plataformas.create(686, 276, 'platShort');
     plataformas.create(686, 364, 'platShort');
+    //techos
+    plataformas.create(158, 305, 'techoApixel');
+    plataformas.create(376, 60, 'techoBpixel');
+    plataformas.create(624, 192, 'techoCpixel');
+    plataformas.create(824, 130, 'techoDpixel');
+    //pared derecha
+    pared.create(925, 215, 'paredPixel');
     //toldos
-    plataformas.create(159, 480, 'toldoLPixel'); //izquierdo 85 646
-    plataformas.create(686, 364, 'toldoRPixel'); //derecho
+    plataformas.create(159, 480, 'toldoLPixel'); //izquierdo
+    plataformas.create(878, 480, 'toldoRPixel'); //derecho
     //tubería
-
+    plataformas.create(605, 116, 'tuboPixel');
     //tanque de agua
+    tanque.create(831, 97, 'tanquePixel');
+    //cartel
+    plataformas.create(60, 385, 'cartelPixel'); //izquierdo
+    plataformas.create(726, 443, 'cartelPixel'); //derecho
+
+
     this.jugador.create();
-    this.physics.add.collider(this.jugador.avatar.sprite, suelo);
+    //colisiones
+    this.physics.add.collider(this.jugador.avatar.sprite, suelo); //con suelo
+    this.physics.add.collider(this.jugador.avatar.sprite, plataformas); //con plataformas
+    this.physics.add.collider(this.jugador.avatar.sprite, pared); //con la pared derecha
+    this.jugador.avatar.sprite.body.collideWorldBounds = true; //con bordes
+    
+    //las plataformas no son colisionables desde abajo (si ejecuta estalla)
+    //this.plataformas.setAll('body.collideDown', false);
+    //plataformas.collideDown=false; //este no hace nada, pero no estalla
+
+    //hacemos los sprites transparentes
+    
+
 	}
 update(){
     this.jugador.update();
     }
 }
-
-	//QUEDAN MÁS PLATAFORMAS POR PONER, DE DISTINTOS TAMAÑOS
