723f72abf5d4486bcf76f2c45896977f9df3ac0f
FernandoMoreno98 <43215893+FernandoMoreno98@users.noreply.github.com>
2018-10-11 10:19:06 +0200

    Plataformas invisibles
    Modificado el atributo alfa de cada plataformas a cero.

--- Fight for the hood/Escenario.js
+++ Fight for the hood/Escenario.js
@@ -4,7 +4,7 @@ class MainEscenario extends Phaser.Scene {
 		super({key:"MainEscenario"});
         var sprite = ["Recursos/Imagenes/Sprite2.png","Recursos/Imagenes/Sprite2.png"]
         this.avatar = new Avatar("a",this,400,400,sprite);
-        this.jugador = new Jugador(this.avatar,controles2);
+        this.jugador = new Jugador(this.avatar,controles2); //elegir controles 1 o controles 2
 	}
 
 
@@ -12,7 +12,7 @@ class MainEscenario extends Phaser.Scene {
 preload(){
 
     this.jugador.preload();
-	this.load.image('fondo', 'Recursos/Imagenes/stage.png')
+	this.load.image('fondo', 'Recursos/Imagenes/stage.png');
 	this.load.image('plat2', 'Recursos/Imagenes/plat2.png'); //atravesables
     this.load.image('platShort', 'Recursos/Imagenes/platShort.png');
     this.load.image('sueloPixel', 'Recursos/Imagenes/sueloPixel.png');
@@ -55,46 +55,47 @@ create(){
 
 	//Suelo
     //platformas.create(x, y, 'suelo').setScale(2).refreshBody();
-    suelo.create(512, 592, 'sueloPixel');
+    suelo.create(512, 592, 'sueloPixel').alpha=0;
 
 
     //LAS COORDENADAS SON RESPECTO A LA VENTANA DEL TAMAÑO DEL ESCENARIO
 
+    //plataformas invisibles
     //escaleras de emergencia
     //izquierda 
-    plataformas.create(319, 63,  'plat2');
-    plataformas.create(319, 153, 'plat2');
-    plataformas.create(319, 241, 'plat2');
-    plataformas.create(319, 328, 'plat2');
-    plataformas.create(319, 416, 'plat2');
+    plataformas.create(319, 63,  'plat2').alpha=0;
+    plataformas.create(319, 153, 'plat2').alpha=0;
+    plataformas.create(319, 241, 'plat2').alpha=0;
+    plataformas.create(319, 328, 'plat2').alpha=0;
+    plataformas.create(319, 416, 'plat2').alpha=0;
     //derecha
-    plataformas.create(866, 141, 'plat2');
-    plataformas.create(866, 229, 'plat2');
-    plataformas.create(866, 317, 'plat2');
-    plataformas.create(866, 404, 'plat2');
+    plataformas.create(866, 141, 'plat2').alpha=0;
+    plataformas.create(866, 229, 'plat2').alpha=0;
+    plataformas.create(866, 317, 'plat2').alpha=0;
+    plataformas.create(866, 404, 'plat2').alpha=0;
     //sueltas
-    plataformas.create(455, 276, 'platShort');
-    plataformas.create(520, 362, 'platShort');
-    plataformas.create(520, 468, 'platShort');
-    plataformas.create(686, 276, 'platShort');
-    plataformas.create(686, 364, 'platShort');
+    plataformas.create(455, 276, 'platShort').alpha=0;
+    plataformas.create(520, 362, 'platShort').alpha=0;
+    plataformas.create(520, 468, 'platShort').alpha=0;
+    plataformas.create(686, 276, 'platShort').alpha=0;
+    plataformas.create(686, 364, 'platShort').alpha=0;
     //techos
-    plataformas.create(158, 305, 'techoApixel');
-    plataformas.create(376, 60, 'techoBpixel');
-    plataformas.create(624, 192, 'techoCpixel');
-    plataformas.create(824, 130, 'techoDpixel');
+    plataformas.create(158, 305, 'techoApixel').alpha=0;
+    plataformas.create(376, 60, 'techoBpixel').alpha=0;
+    plataformas.create(624, 192, 'techoCpixel').alpha=0;
+    plataformas.create(824, 130, 'techoDpixel').alpha=0;
     //pared derecha
-    pared.create(925, 215, 'paredPixel');
+    pared.create(925, 215, 'paredPixel').alpha=0;
     //toldos
-    plataformas.create(159, 480, 'toldoLPixel'); //izquierdo
-    plataformas.create(878, 480, 'toldoRPixel'); //derecho
+    plataformas.create(159, 480, 'toldoLPixel').alpha=0; //izquierdo
+    plataformas.create(878, 480, 'toldoRPixel').alpha=0; //derecho
     //tubería
-    plataformas.create(605, 116, 'tuboPixel');
+    plataformas.create(605, 116, 'tuboPixel').alpha=0;
     //tanque de agua
-    tanque.create(831, 97, 'tanquePixel');
+    tanque.create(831, 97, 'tanquePixel').alpha=0;
     //cartel
-    plataformas.create(60, 385, 'cartelPixel'); //izquierdo
-    plataformas.create(726, 443, 'cartelPixel'); //derecho
+    plataformas.create(60, 385, 'cartelPixel').alpha=0; //izquierdo
+    plataformas.create(726, 443, 'cartelPixel').alpha=0; //derecho
 
 
     this.jugador.create();
@@ -107,12 +108,9 @@ create(){
     //las plataformas no son colisionables desde abajo (si ejecuta estalla)
     //this.plataformas.setAll('body.collideDown', false);
     //plataformas.collideDown=false; //este no hace nada, pero no estalla
-
-    //hacemos los sprites transparentes
-    
-
 	}
 update(){
     this.jugador.update();
+
     }
 }
