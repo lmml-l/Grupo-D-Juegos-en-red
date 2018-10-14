8a82ce6a04e8c7619d9d0882aae3ff43c1d53bdf
Alex <43203158+Tenusiano@users.noreply.github.com>
2018-10-14 10:33:02 +0200

    Clase HUD
    Primera plantilla de clase HUD a la espera de hacerlo junto a la clase arma y barras de vida

---
+++ Fight for the hood/HUD.js
@@ -0,0 +1,56 @@
+class HUD extends Phaser.Scene {
+	constructor(){
+		super({key:"HUD"});
+		//this.vida = new                       //Recibe la vida de la clase personaje 
+		//this.munición = new                   //Recibe la munición de la clase arma
+		this.arma = new Arma (this.escena,arma);//Recibe el arma de la clase arma
+
+		
+		
+	}
+	preload ()
+	{
+		this.load.image('arma1', 'Recursos/Imagenes/...');      //Carga cada cosa del HUD
+		this.load.image('arma2', 'Recursos/Imagenes/...'); 
+		this.load.image('arma3', 'Recursos/Imagenes/...'); 
+		this.load.image('arma4', 'Recursos/Imagenes/...'); 
+		this.load.image('arma5', 'Recursos/Imagenes/...');     
+		this.load.image('municion', 'Recursos/Imagenes/...');
+		this.load.image('vida', 'Recursos/Imagenes/');
+		this.load.image('FondoHUD', 'Recursos/Imagenes/...');
+		
+		
+	}
+
+	create ()
+	{
+
+
+		
+		this.add.sprite(512, 215, 'FondoHUD'); //sprite del escenario
+		
+		this.add.image(400, 300, 'sky');           
+		this.vida.create();
+
+		this.add.image(400, 300, 'sky');
+		this.munucion.create();
+
+		this.add.image(400, 300, 'sky');
+		this.arma.create();
+
+		
+		
+		//this.avatar.walkright();
+
+		//this.sprite = new Phaser.Gameobjects.Sprite(this,100,100,'p1l',2).setScale(4);
+		//var a = this.add.image(150,150,'p1l').setScale(1);
+	}
+
+	update ()
+	{
+	  	this.arma.update()
+	  	this.municion.update()
+	  	this.vida.update()
+
+	}
+}
