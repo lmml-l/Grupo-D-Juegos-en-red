985706630e871fda2da273f96a0e916c32276229
FernandoMoreno98 <43215893+FernandoMoreno98@users.noreply.github.com>
2018-10-20 23:24:50 +0200

    Corrección de drops
    Corregidas las posiciones

--- Fight for the hood/Drops.js
+++ Fight for the hood/Drops.js
@@ -13,11 +13,11 @@ function Drops(escena, sprites) {
 		//posición aleatoria
 		var r = Math.floor(Math.random() * 5)  ; //las posiciones que sean (de momento son 2 máximos) (de 0 a 2?)
 		//posiciones de los drops
-		var pos1 = new Array(252,224);
-		var pos2 = new Array(452,324);
-		var pos3 = new Array(62,324);
-		var pos4 = new Array(292,378);
-		var pos5 = new Array(732,324);
+		var pos1 = new Array(135, 492);
+		var pos2 = new Array(505, 46);
+		var pos3 = new Array(460, 255);
+		var pos4 = new Array(862, 118);
+		var pos5 = new Array(691, 343);
 		//array de posiciones posibles
 		var arrayPos = new Array(pos1, pos2, pos3, pos4, pos5);
 		//asignación de posición
@@ -81,61 +81,11 @@ function Drops(escena, sprites) {
 		var armapw = that.scene.physics.add.sprite(armapos[0],armapos[1], that.selectID()).setScale(1);
 		that.sprite.push(armapw)
 		}
-		//var i = Math.floor(Math.random(5));
-		//var s = that.sprite[i];
-		//if(that.sprite[i]!=undefined){
-			//s.destroy();
-		//}
 		
-		//that.sprite[i]=armapw;
 
 		if(that.sprite.length > 5){
 			var s = that.sprite.shift();
 			s.destroy();
 		}
-		
-		//return armapw;
 	}
-
-	/*this.printWeapon = function(idarma){
-		switch(idarma){
-			case esc:
-				that.load.spritesheet('Escopeta',escopeta);
-				that.newArma = "escopeta";
-			break;
-				
-			case pst:
-				that.load.spritesheet('Pistola',pistola);
-				that.newArma = "pistola";
-			break;
-
-			case sbf:
-				that.load.spritesheet('Subfusil',subfusil);
-				that.newArma = "subfusil";
-			break;
-
-			case pa:
-				that.load.spritesheet('PuñoAmericano',puñoamericano);
-				that.newArma = "puño americano";
-			break;
-
-			case bb:
-				that.load.spritesheet('BateBeisbol',batebeisbol);
-				that.newArma = "bate de beisbol";
-			break;
-		}	
-	}
-	*/
-
-	/*this.pickndrop = function(arma, scene, newArma){
-		this.callbackID = this.newArma;
-		this.sprite.destroy();
-		printWeapon(arma);
-	}
-	*/
-
-	/*this.setnewID = function(){
-		this.Jugador.arma = this.callbackID;
-	}
-	*/
 }
