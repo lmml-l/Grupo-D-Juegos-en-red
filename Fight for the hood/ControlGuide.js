434273e6f1887147ac175769072c0b745fee988e
KaptainSportaco <43203165+KaptainSportaco@users.noreply.github.com>
2018-10-30 02:09:22 +0100

    Cambios en el menu, sprites subidos y creación de clase de guia de controles

---
+++ Fight for the hood/ControlGuide.js
@@ -0,0 +1,57 @@
+class ControlGuide extends Phaser.Scene {
+	constructor(){
+		super({key:"ControlGuide"});
+		this.check;
+		this.back1;
+		this.back2;
+	}
+	
+	controlmenu(){
+
+		this.check = false;
+
+		var controls = this.add.image(this.game.canvas.width/2,this.game.canvas.height/2,'controls').setScale(1);
+
+		this.back1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACK);
+		this.back2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
+		this.back1.isDown=false;
+		this.back2.isDown=false;
+
+
+
+		
+	}
+	preload(){
+		var controls = this.load.image('controls','Recursos/Imagenes/GuiaControles.png');
+	}
+
+	create(){
+		this.controlmenu();
+	}
+
+	actualizarPosArray(){	
+		if(this.back1.isDown){
+			this.check = true;
+			
+		}
+
+		if(this.back2.isDown){
+			this.check = true;
+		}
+		//console.log("aaaa")
+	}
+
+	scenechange(){
+		if(this.check){
+			this.scene.start('MainMenu');
+		}
+	}
+
+
+	update(){
+		this.scenechange();
+		this.actualizarPosArray();
+
+	}
+}
--- Fight for the hood/menu.js
+++ Fight for the hood/menu.js
@@ -1,24 +1,60 @@
 class MainMenu extends Phaser.Scene {
 	constructor(){
 		super({key:"MainMenu"});
+		this.smap = new Array();
+		this.check;
+		this.selector;
+		this.posArray;
+		this.arriba1;
+		this.abajo1;
+		this.go1;
+		this.arriba2;
+		this.abajo2;
+		this.go2;
 	}
 	
 	menu(){
+
+		this.check = false;
+
+		this.posArray=0;
+
 		var title = this.add.image(0,this.game.canvas.height*(150/600),'title').setScale(0.5);
 		var buttonlocal = this.add.image(0,this.game.canvas.height*(3/6),'buttonlocal').setScale(0.5).setInteractive();
 		var buttononline = this.add.image(0,this.game.canvas.height*(4/6),'buttononline').setScale(0.5).setInteractive();
 		var controles = this.add.image(0,(this.game.canvas.height*(5/6)),'controles').setScale(0.5).setInteractive();
 		
+		this.smap.push(buttonlocal);
+		this.smap.push(buttononline);
+		this.smap.push(controles);
+
+		this.selector = this.add.sprite((this.smap[this.posArray].x + 800), this.smap[this.posArray].y, 'Selector').setScale(1);
+
+		/*
 		buttonlocal.on('pointerdown', function(event){this.scene.start('CharapterSelection')},this); 
 		buttononline.on('pointerdown', function(event){this.scene.start('CharapterSelection')},this);
 		controles.on('pointerdown', function(event){this.scene.start('CharapterSelection')},this); 
-
-		var container = this.add.container(0,0);
+		*/
+	
+		var container = this.add.container(this.game.canvas.width/2, 0);
 		container.add(title);
 		container.add(buttonlocal); 
 		container.add(buttononline); 
 		container.add(controles);  
 		container.setX(this.game.canvas.width/2);
+
+
+		this.arriba1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+		this.arriba2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+		this.abajo1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+		this.abajo2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+		this.go1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
+		this.go2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+
+		this.go1.isDown=false;
+		this.go2.isDown=false;
+
+
 		
 	}
 	preload(){
@@ -26,11 +62,50 @@ class MainMenu extends Phaser.Scene {
 		var buttonlocal = this.load.image('buttonlocal','Recursos/Imagenes/Jugar_Local.png');
 		var buttononline = this.load.image('buttononline','Recursos/Imagenes/Jugar_Online.png');
 		var controles = this.load.image('controles','Recursos/Imagenes/Controles.png');
+		var selector = this.load.image('Selector','Recursos/Imagenes/Selector.png');
 	}
 
 	create(){
 		this.menu();
 	}
 
-	
+	actualizarSelector(){
+		this.selector.setPosition(this.smap[this.posArray].x + 800, this.smap[this.posArray].y);
+	}
+
+	actualizarPosArray(){	
+		if(this.abajo1.isDown && (this.posArray<2) && !this.check){
+			this.posArray++;
+		}else if(this.arriba1.isDown && (this.posArray>0) && !this.check){
+			this.posArray--;
+		}else if(this.go1.isDown){
+			this.check = true;
+			
+		}
+
+		if(this.abajo2.isDown && (this.posArray<2) && !this.check){
+			this.posArray++;
+		}else if(this.arriba2.isDown && (this.posArray>0) && !this.check){
+			this.posArray--;
+		}else if(this.go2.isDown){
+			this.check = true;
+		}
+		//console.log("aaaa")
+	}
+
+	scenechange(){
+		if(this.posArray<2 && this.check){
+			this.scene.start('CharapterSelection');
+		}else if(this.posArray=2 && this.check){
+			this.scene.start('ControlGuide');
+		}
+	}
+
+
+	update(){
+		this.scenechange();
+		this.actualizarSelector();
+		this.actualizarPosArray();
+
+	}
 }
