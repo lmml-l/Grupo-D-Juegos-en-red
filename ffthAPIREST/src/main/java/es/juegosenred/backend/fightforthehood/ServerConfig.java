2a9e66538cc8cadbb1c03136b0a0ffa8adbdfdd5
Alex <43203158+Tenusiano@users.noreply.github.com>
2018-11-24 18:31:43 +0100

    Clase que permite que no salte el cors policy
    Clase que permite que no salte el error de cors policy al ejecutar el servidor

---
+++ ffthAPIREST/src/main/java/es/juegosenred/backend/fightforthehood/ServerConfig.java
@@ -0,0 +1,22 @@
+package es.sidelab.EscapeBandages;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+// Importamos una serie de bibliotecas para evitar el CORS POLICY 
+@Configuration
+public class ServerConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST","PUT","DELETE");
+            }
+        };
+    }
+}
