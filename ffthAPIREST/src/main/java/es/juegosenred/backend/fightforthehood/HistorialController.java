be008c201de5a311a0cbee4a465bf2d558d5238d
KaptainSportaco <43203165+KaptainSportaco@users.noreply.github.com>
2018-11-27 04:14:55 +0100

    Cambios en el guardado de datos persistentes

--- ffthAPIREST/src/main/java/es/juegosenred/backend/fightforthehood/HistorialController.java
+++ ffthAPIREST/src/main/java/es/juegosenred/backend/fightforthehood/HistorialController.java
@@ -1,7 +1,11 @@
 package es.juegosenred.backend.fightforthehood;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +20,21 @@ public class HistorialController {
 	
 	@PutMapping("/lastmatch")
 	@ResponseStatus(HttpStatus.CREATED)
-	public String putIp(@RequestBody String batalla){	
-	mymatch.getHistorial().add(batalla);
-	ReaderWriter.Escritor(mymatch.getHistorial());
-	System.out.println(mymatch.getHistorial().size());
-	return batalla;
+	public String putLastMatch(@RequestBody String batalla){	
+		mymatch.getHistorial().add(batalla);
+		ReaderWriter.Escritor(mymatch.getHistorial());
+		System.out.println(mymatch.getHistorial().size());
+		return batalla;
+	}
+	
+	@GetMapping("/lastmatches")
+	@ResponseStatus(HttpStatus.CREATED)
+	public String[] getLastMatches(@PathVariable String[] historial ){
+		historial = new String[5];
+		for(int i = 0; i<5; i++) {
+			historial[i] = mymatch.getHistorial().get(i);
+			System.out.println(mymatch.getHistorial().get(i));
+		}
+		return historial;
 	}
 }
