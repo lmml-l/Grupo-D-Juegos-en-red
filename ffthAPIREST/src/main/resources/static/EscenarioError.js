4353c21c454622ba368431b6b34ed8ca5f459ebe
FernandoMoreno98 <43215893+FernandoMoreno98@users.noreply.github.com>
2018-11-28 13:30:02 +0100

    Corrección de código
    La entrega ahora funciona, tanto en el .jar como en el código en Spring Tool Suite

--- ffthAPIREST/src/main/resources/static/EscenarioError.js
+++ ffthAPIREST/src/main/resources/static/EscenarioError.js
@@ -10,57 +10,30 @@ constructor(){
 }
 
 preload(){
-        this.load.image('menuNombreFondo','Recursos/Imagenes/menuNombreFondo.png');
+        
     }
 
 create (){
 
-    this.fondo = this.add.image(this.game.canvas.width/2,this.game.canvas.height/2,'menuNombreFondo').setScale(1.3);
+  
     this.esc   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);    //tecla para salir del menú de error
     this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER); //tecla para salir el menú de error y salir
+    
 
 
     this.texto = this.add.text(50, 300, "You have been disconnected from the server.", { fill: '#9B111E', font: '48px Impact', align: 'center'})
     this.texto = this.add.text(50, 700, "Restart this page.", { fill: '#F4FFF3', font: '16px Impact', align: 'center'});
-
-	constructor(){
-		super({key:"EscenarioError"});
-		this.esc;
-		this.enter;
-//botón para retroceder
 }
-	retroceder(){
-		if(esc.isDown){
-		this.scene.start('MainMenu');
-		}
-	}
-
-	aceptar(){
-		if(enter.isDown){
-			this.scene.start('MainMenu');
-		}
-		
-	}
+	
 
 
-create (){
-
-    this.esc   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);	//tecla para salir del menú de error
-    this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER); //tecla para salir el menú de error y salir
 
 
-    this.textoVersion = this.add.text(500, 500, "Te has desconectado del servidor.", { fill: '#9B111E', font: '24px Lucida Console', align: 'center'});
-
-
-     }
 
 
 
 update(){
 
-    }
-		this.retroceder();
-		this.aceptar();
 	}
 
 
