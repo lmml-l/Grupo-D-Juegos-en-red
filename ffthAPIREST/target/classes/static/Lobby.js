29ede8f6187a37375100098a2e4e05dcb2af1e14
KaptainSportaco <43203165+KaptainSportaco@users.noreply.github.com>
2018-11-28 03:01:38 +0100

    Ajuste maquetación del lobby y petición

--- ffthAPIREST/target/classes/static/Lobby.js
+++ ffthAPIREST/target/classes/static/Lobby.js
@@ -1,4 +1,3 @@
-
 var ipsLobby = new Array();
 var partidaactual;
 class Lobby extends Phaser.Scene {
@@ -23,6 +22,8 @@ class Lobby extends Phaser.Scene {
 		this.scene;
 		this.fondo;
 		this.salir;
+		this.ready1;
+		this.ready2;
 		this.ready;
 	}
 	
@@ -32,8 +33,8 @@ class Lobby extends Phaser.Scene {
 		this.check1 = false;
 		this.check2 = false;
 
-		this.nombreRival[0]="?"
-		this.nombreRival[1]="Fulanita123456";
+		this.nombreRival[0]="???"
+		this.nombreRival[1]="???";
 
 		//Valor de conexión
 		this.estadoConexion[0]="disconnected";
@@ -41,13 +42,13 @@ class Lobby extends Phaser.Scene {
 
 		//Buscando jugador o ya encontrado
 		this.estadoBusqueda[0]="Looking for rivals...";
-		this.estadoBusqueda[1]="READY\nPRESS SPACE TO GO";
+		this.estadoBusqueda[1]="PRESS SPACE\nTO ACCEPT";
 
 		//Textos
 		this.textModo      = this.add.text(50, 50, "Online Mode", { fill: '#F4FFF3', font: '20px Impact', align: 'center'});
 		this.textoSalir    = this.add.text(50, 730, "ESC to exit", { fill: '#F4FFF3', font: '24px Impact', align: 'center'});
 		this.textoConexion = this.add.text(650, 730, "Connection status: " + this.estadoConexion[1], { fill: '#F4FFF3', font: '24px Impact', align: 'center'});
-		this.textoBusqueda = this.add.text(380, 480, this.estadoBusqueda[1], { fill: '#FFFFFF', font: '36px Impact', align: 'center'});
+		this.textoBusqueda = this.add.text(380, 480, this.estadoBusqueda[0], { fill: '#FFFFFF', font: '36px Impact', align: 'center'});
 
 		//PARTIDAS POR DEFECTO/////////////////////////////
 		this.historialPartidas[0]="-";
@@ -68,49 +69,16 @@ class Lobby extends Phaser.Scene {
 		////////////////////////
 		this.texts[0] = this.add.text(312, 400, "" ,{ fill: '#FFFFFF', font: '36px Impact', align: 'center'});
 		this.texts[1] = this.add.text(490, 370, "VS", { fill: '#FFFFFF', font: '54px Impact', align: 'center'});
-		this.texts[2] = this.add.text(80, 370, this.nombreRival[0], { fill: '#FFAC00', font: '54px Impact', align: 'center'}); //máximo de 15 letras
-		this.texts[3] = this.add.text(600, 370, this.nombreRival[1], { fill: '#FFAC00', font: '54px Impact', align: 'center'});
+		this.texts[2] = this.add.text(80, 370, this.nombreRival[0], { fill: '#FFAC00', font: '54px Impact', align: 'right'}); //máximo de 15 letras
+		this.texts[3] = this.add.text(600, 370, this.nombreRival[1], { fill: '#FFAC00', font: '54px Impact', align: 'left'});
 		this.texts[4] = this.add.text(320, 190, "PREPARE TO FIGHT!", { fill: '#FFFFFF', font: '54px Impact', align: 'center'});
 		////////////////////////
-	/*
-		this.texts[0] = this.add.text(312, 400, "" ,{ fill: '#FFFFFF', font: '18px Impact', align: 'center'}).setScale(2);
-		this.texts[1] = this.add.text(712, 400, "this.getRivalIp(mymatch)", { fill: '#FFFFFF', font: '18px Impact', align: 'center'}).setScale(2);
-		this.texts[2] = this.add.text(312, 600, "this.getApodo(this.mymatch", { fill: '#FFFFFF', font: '18px Impact', align: 'center'}).setScale(2);
-		this.texts[3] = this.add.text(712, 600, "this.getApodo(this.mymatch", { fill: '#FFFFFF', font: '18px Impact', align: 'center'}).setScale(2);
-		this.texts[4] = this.add.text(512, 100, "Partida 1v1", { fill: '#FFFFFF', font: '30px Impact', align: 'center'}).setScale(2);
-	*/
-		//this.listlp = this.getListaApodos();
 
 		this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 		this.back = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKSPACE);
 
 		this.enter.isDown=false;
 		this.back.isDown=false;
-	}
-
-	/*
-	enfrentamientoactual(){
-		if(ipsLobby.length == 2){
-			if(partidaactual != ipsLobby){//partida actual se refiere a las ips que hemos mandado antes al historial
-				addMatchtoHistory(ipsLobby[0] + " vs " + ipsLobby[1]);
-				partidaactual=ipsLobby
-			}
-		}
-	}
-	*/
-
-	comprobaripssala(ipsconectadas,ipslobby){
-		var ipsensalapresentesenserver = new Array();
-		for(var i = 0; i < ipsconectadas.length ; i++){
-			for(var j = 0 ; j < ipslobby.length ; j++){
-				console.log( "ipone "+ ipsconectadas[i])
-				console.log("jpone" + ipslobby[j])
-				if(ipsconectadas[i] == ipslobby[j]){
-					ipsensalapresentesenserver.push(ipslobby[j]);
-				}
-			}
-		}
-		ipsLobby = ipsensalapresentesenserver;
 	}
 
 	preload(){
@@ -120,7 +88,6 @@ class Lobby extends Phaser.Scene {
 	create(){
 		this.fondo = this.add.image(this.game.canvas.width/2,this.game.canvas.height/2,'menuLobbyFondo').setScale(1.3);
 		this.escape = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);	//tecla para salir
-		this.ready = this.add.text(650/*RELATIVO*/, 540/*RELATIVO*/, "\n\n\nREADY", { fill: '#FFFFFF', font: '32px Impact', align: 'center'});
 		this.controlmenu();
 
 		var that = this;
@@ -128,22 +95,6 @@ class Lobby extends Phaser.Scene {
 		this.time.addEvent({delay:100, loop:true,
     	callback: function(){getServerStatus(function(){that.scene.start('EscenarioError');})}})
 
-		///////
-		var miip = ip;
-		 //Si estoy conectado consigo mi ipactual
-
-		console.log(miip + "miip");
-
-		this.time.addEvent({delay:50,loop:true, callback: function(){addIptoIpConectadas(miip)}});//Añade la ip a las conectadas
-
-		var listadeipsconectadas;
-
-		this.time.addEvent({delay:80,loop:true, callback: function(){getIpsConectadas(function(data){listadeipsconectadas = data})}})
-
-		this.time.addEvent({delay:150,loop:true, callback: function(){that.comprobaripssala(listadeipsconectadas,ipsLobby);}})
-
-		this.time.addEvent({delay:300,loop:true, callback: function(){addIptoIpConectadasClear()}})//resetea ips conectadas al servidor
-		///
 	}
 
 	//botón para retroceder
@@ -156,30 +107,26 @@ class Lobby extends Phaser.Scene {
 	}
 	
 	avanzar(){
-		/*if(this.enter.isDown){
-			putcheckReady();
+		if(this.enter.isDown){
+			putcheckReady(true);
 			if(getcheckReady()){
+				this.ready1 = this.add.text(397,5, 370, "\n\n\nREADY", { fill: '#FFFFFF', font: '32px Impact', align: 'right'});
 				this.enter.isDown = false;
 				that.scene.start('MainEscenario');
 			}
 			
-		}*/
+		}
 	}
-
+	
 
 	update(){
 		this.retroceder();
-		//this.scenechange();
+		this.avanzar();
 
 		var that=this;
 		
 		getIPs(function(arrayjugadores){ipsLobby= arrayjugadores});//ips jugadores en la sala
 
-		
-
-
-
-		////////////////////////////////////
 		this.time.addEvent({delay:250,  //se tarda un poco en actualizar en nombre del primer jugador de la sala
     	callback: function(){getApodo(function(data){that.nombreRival[0]=data},ipsLobby[0].substring(1,ipsLobby[0].length-1));}})
 
@@ -192,11 +139,16 @@ class Lobby extends Phaser.Scene {
 
 		//Se tarda un tiempo en tener el segundo nombre por eso se tarda en actualizar
     	this.time.addEvent({delay:250,  //tiempo que tarda hasta reiniciar
-    	callback: function(){getApodo(function(data){that.nombreRival[1]=data},url2());}})
+    	callback: function(){getApodo(function(data){that.nombreRival[1]=data;},url2());}})
 		
     	//Se cambian los contenidos de los  textos que muestran los nombres por los apodos de los jugadores actuales.
 		this.texts[2].text=this.nombreRival[0];
 		this.texts[3].text=this.nombreRival[1];
+		if(this.texts[3].text != ""){
+			this.textoBusqueda.text=this.estadoBusqueda[1];
+		}else{
+			this.textoBusqueda.text=this.estadoBusqueda[0];
+		}
 
 		this.time.addEvent({delay:250,  //tiempo que tarda hasta reiniciar
     	callback: function(){getHistorial(function(data){that.historialPartidas = data})}})
