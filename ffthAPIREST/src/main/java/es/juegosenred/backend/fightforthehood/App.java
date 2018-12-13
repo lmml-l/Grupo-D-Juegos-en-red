4b390aa4f58d447ad48a4f93d22b13621aefedd8
KaptainSportaco <43203165+KaptainSportaco@users.noreply.github.com>
2018-12-13 10:06:02 +0100

    Configuración Websockets

--- ffthAPIREST/src/main/java/es/juegosenred/backend/fightforthehood/App.java
+++ ffthAPIREST/src/main/java/es/juegosenred/backend/fightforthehood/App.java
@@ -3,15 +3,31 @@ package es.juegosenred.backend.fightforthehood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class App 
+@EnableWebSocket
+public class App implements WebSocketConfigurer
 {
 	
 	@Bean
 	public MyMatch mymatch() {
 		return new MyMatch();
 	}
+	
+	@Override
+	public void registerWebSocketHandlers(
+	WebSocketHandlerRegistry registry) {
+	registry.addHandler(echoHandler(), "/echo")
+	.setAllowedOrigins("*");
+	}
+	@Bean
+	public WebsocketEchoHandler echoHandler() {
+	return new WebsocketEchoHandler();
+	}
+
 	
 	
     public static void main( String[] args )
