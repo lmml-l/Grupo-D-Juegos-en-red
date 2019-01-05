9ef82c74dc53b829c715fb2f0f213d2bdd6f443a
FernandoMoreno98 <fernando.morenodiaz@hotmail.com>
2019-01-05 12:47:17 +0100

    Eliminación modo online (excepto menú)
    Elimino toda referencia a API REST o websockets para volver a tener un juego en local

---
+++ Fight for the hood/src/main/resources/static/game.js
@@ -0,0 +1,31 @@
+'use strict'
+var config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768, //768
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    //Orden de escenas
+    scene: [MainMenu,ControlGuide,CharapterSelection,MainEscenario] //scene: [MainMenu,ControlGuide,nameMenu,Lobby,CharapterSelection,MainEscenario]
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+
+}
+
+function create ()
+{
+
+}
+
+function update ()
+{
+}
