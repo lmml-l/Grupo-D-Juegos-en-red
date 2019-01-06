1152c3b7e9ae1abdfe5cfb129107597408709a19
Alex <43203158+Tenusiano@users.noreply.github.com>
2019-01-06 23:15:16 +0100

    Cambios nombre de carpeta y JAR FILE
    Cambios nombre de carpetas y adjutnado el archivo JAR

---
+++ ffthWEBSOCKETS/src/main/java/es/juegosenred/backend/fightforthehood/MyInfo.java
@@ -0,0 +1,29 @@
+package es.juegosenred.backend.fightforthehood;
+
+public class MyInfo {
+
+	private String ip;
+	private String apodo;
+	private float resptime;
+	
+	
+	public String getApodo() {
+		return apodo;
+	}
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public float getResptime() {
+		return resptime;
+	}
+	public void setResptime(float resptime) {
+		this.resptime = resptime;
+	}
+	
+}
---
+++ ffthWEBSOCKETS/target/classes/static/requests.js
@@ -0,0 +1,260 @@
+
+//Variable con la dirección IP del servidor
+var IPserver = location.host;
+
+//Añadir informacion propia
+function putMyInfo(myinfo) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://'+ IPserver+ '/myinfo/' + myinfo.ip,
+        data: JSON.stringify(myinfo),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("Player data  IP/Apodo: " + JSON.stringify(myinfo));
+    })
+}
+
+function putMyResponseTime(myinfo, resptime) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://'+ IPserver+ '/myinfo/' + myinfo.ip + '/resptime/',
+        data: JSON.stringify(resptime),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("ResponseTime: " + JSON.stringify(resptime));
+    })
+}
+
+function putMyRTToZero(myinfo) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://'+ IPserver+ '/myinfo/' + myinfo.ip + '/resptime/',
+        data: JSON.stringify(0),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("ResponseTime set to zero: " + JSON.stringify(0));
+    })
+}
+
+function getMyResponseTime(myinfo, request) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://'+ IPserver+ '/myinfo/' + myinfo.ip + '/resptime/',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        request(data);
+        console.log("ResponseTime: " + data)
+    })
+}
+
+function addPlayertoRoom (ip) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://'+ IPserver +'/ips/id',
+        data: JSON.stringify(ip),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("Ip del jugador añadido " + ip);
+    })
+}
+
+function deletePlayerofRoom (ip) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://' + IPserver + '/ips/deleteid',
+        data: JSON.stringify(ip),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("Ip del jugador borrado" + ip);
+    })
+}
+
+function addMatchtoHistory(match) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://' + IPserver + '/lastmatch',
+        data: JSON.stringify(match),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("Ip del jugador añadido " + JSON.stringify(match));
+    })
+}
+
+function addIptoIpConectadas(ip) {
+    $.ajax({
+        method: "PUT",
+        url: 'http://' + IPserver + '/ips/ipconectados',
+        data: JSON.stringify(ip),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("ip añadida " + JSON.stringify(ip));
+    })
+}
+
+function getIpsConectadas(request) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/ips/idlistconectadas',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        request(data);
+        console.log("Info: " + data)
+    })
+}
+
+function addIptoIpConectadasClear() {
+    $.ajax({
+        method: "PUT",
+        url: 'http://' + IPserver + '/ips/ipconectadosclear',
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("ips conectadas reseteadas ");
+    })
+}
+
+
+function getMyInfo(request) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/myinfo',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        request(data);
+        console.log("Info: " + data)
+    })
+}
+
+
+//Conseguir apodo 
+function getApodo(request,ip) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/apodos/apodolist/'+ ip,
+    }).done(function (data) {
+        request(data);
+        console.log("Apodo: " + data)
+        console.log("IPsdsdsd: "+ip)
+    })
+}
+
+//Conseguir IP propia
+function getMyIP(request) {
+    $.ajax({
+        method: 'GET',
+        url: 'https://ipapi.co/json/',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        request(data);
+        console.log("IP Propia: " + data)
+    })
+}
+
+//Saber estado de la conexión con el servidor
+function getServerStatus(error) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/historial',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).fail(function (data) {
+        console.log("Error: Servidor desconectado")
+        error()
+    })
+}
+
+function getServerStatusCheck(checking) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/historial',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        checking()
+    })
+}
+
+//Conseguir lista de IPs
+function getIPs(arraips) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/ips/idlist',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        console.log("IPs en partida: " + data)
+        arraips(data);
+    })
+}
+
+function getHistorial(request) {
+    $.ajax({
+        method: 'GET',
+        url: 'http://' + IPserver + '/historial',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        console.log("Historial: " + data)
+        request(data);
+    })
+}
+
+function putTime(ip,id){
+     $.ajax({
+        method: "PUT",
+        url: 'http://' + IPserver + '/ips/time/' + ip,
+        data: JSON.stringify(id),
+        processData: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function () {
+        console.log("ip tiempo" + JSON.stringify(ip));
+    })
+}
+
+/*Delete item from server
+function deleteItem() {
+    $.ajax({
+        method: 'DELETE',
+        url: 'http://localhost:8080/FightForTheHood/'
+    }).done(function (item) {
+        console.log("Deleted item " + itemId)
+    })
+    */
+//}
